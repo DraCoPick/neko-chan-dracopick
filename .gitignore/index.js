71118de26cbbb1025bf3e82f6c4e6f33dff7b629
DraCoPick <45017204+DraCoPick@users.noreply.github.com>
2018-11-21 20:11:47 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,15 +8,42 @@ bot.on('ready', function() {
     console.log("Connecté");
 });
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.TOKEN)
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Quoi ? Un rescapé !? Viens trouver refuge chez nous ' + member.displayName);
+    }).catch(console.error)
 
 bot.on('message', message =>{
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes : \n -nc/help");
-    }
+        message.channel.send( {embed: {
+            color: 3447003,
+            author: {
+                name:bot.user.username,
+                icon_url: bot.user.avatarURL
+            },
+            title: "Commandes:",
+            url: 'https://twitter.com/NekoCha88477547',
+            description : `**Liste des commandes :** \n -ncb/help \n -ncb/ping`,
+            fields: [{
+                name:'Update',
+                value:'Ajout du embed du /help  \n Ajout du lien Twitter'
+            }],
+            timestamp: new Date(),
+            footer:{
+                icon_url:bot.user.avatarURL,
+                text: 'Par DraCoPick'
+            }
+        }});
 
     if (message.content === "Salut"){
         message.reply("Nyah Nyah Nyah <3");
         console.log("Commande Nyah effectué");
     }
-})
+
+    if(message.content === prefix + "ping"){
+        message.channel.send("Pong");
+    }
+
+});
